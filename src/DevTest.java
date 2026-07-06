b0aaa6fa37428cd5cd4dcc61febfc53abc176769
delaney-clay <claydk@mail.uc.edu>
2026-07-06 19:26:53 -0400

    finished 2nd method commit

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class DevTest
+{
+    void main()
+    {
+        Scanner in = new Scanner (System.in);
+        String Name = "";
+        int Age = 0;
+
+        Name = SafeInput.getNonZeroLenString(in, "Enter your name");
+        System.out.println("Your name is " + Name + ".");
+
+        Age = SafeInput.getInt(in, "Enter your age");
+        System.out.println("Your age is " + Age + ".");
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class SafeInput {
+public class SafeInput
+{
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = "";
+        do {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+        } while (retString.length() == 0);
+
+        return retString;
+
+    }
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retInt = 0;
+        boolean done = false;
+        String trash = "";
+
+        do {
+            System.out.print("\n" + prompt + ": ");
+            if (pipe.hasNextInt())
+            {
+                retInt = pipe.nextInt();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.next();
+                System.out.println("You entered: " + trash + ". Please enter a valid integer.");
+                done = false;
+            }
+            pipe.nextLine();
+        } while (!done);
+
+        return retInt;
+    }
 }
