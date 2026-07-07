8ef91c30d7dec59d7524d961aaf9b6dc137bc4fc
delaney-clay <claydk@mail.uc.edu>
2026-07-06 21:23:01 -0400

    finished FavNumbers.java commit

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class FavNumbers
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner (System.in);
+        int favInt = 0;
+        double favDouble = 0.0;
 
+        favInt = SafeInput.getInt(in, "What is your favorite integer?");
+        System.out.println("Your favorite integer is " + favInt + ".");
+        favDouble = SafeInput.getDouble(in, "What is your favorite double?");
+        System.out.println("Your favorite double is " + favDouble + ".");
     }
 }
